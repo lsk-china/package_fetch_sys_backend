acc6164ba6995f9e06efbcd2e1798c4a24d95742
lsk-china <3075929352@qq.com>
2024-07-20 09:51:36 +0800

    initial commit

---
+++ src/main/java/com/lsk/packagefetch/PackageFetchApplication.java
@@ -0,0 +1,13 @@
+package com.lsk.packagefetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageFetchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PackageFetchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lsk/packagefetch/PackageFetchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsk.packagefetch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageFetchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
